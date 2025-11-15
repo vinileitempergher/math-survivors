183836f8d16711655212672cc660d3b4144a07d0
ValdeciNovak <valdecirnovak@gmail.com>
2025-11-15 00:22:30 -0300

    Ajustes

--- Utility/enemy_spawner.gd
+++ Utility/enemy_spawner.gd
@@ -1,54 +1,72 @@
 extends Node2D
 
-
 @export var spawns: Array[Spawn_info] = []
 
 @onready var player = get_tree().get_first_node_in_group("player")
 
 @export var time: int = 0
-@export var max_enemies: int = 150
-@export var cull_distance: float = 800.0
-
-var enemy_count: int = 0
+@export var max_enemies: int = 300       # << LIMITE DE INIMIGOS
+@export var cull_distance: float = 6000.0
 
 signal changetime(time)
 
 func _ready():
-	connect("changetime",Callable(player,"change_time"))
+	connect("changetime", Callable(player, "change_time"))
 
 func _on_timer_timeout():
 	time += 1
-	cull_distant_enemies()
-	var enemy_spawns = spawns
-	for i in enemy_spawns:
-		if time >= i.time_start and time <= i.time_end:
-			if i.spawn_delay_counter < i.enemy_spawn_delay:
-				i.spawn_delay_counter += 1
-			else:
-				i.spawn_delay_counter = 0
-				var new_enemy = i.enemy
-				var counter = 0
-				while counter < i.enemy_num and enemy_count < max_enemies:
-					var enemy_spawn = new_enemy.instantiate()
-					var spawn_pos = get_random_position()
-					if is_position_valid(spawn_pos):
-						enemy_spawn.global_position = spawn_pos
-						enemy_spawn.connect("remove_from_array", Callable(self, "_on_enemy_removed"))
-						add_child(enemy_spawn)
-						enemy_count += 1
-					counter += 1
-	emit_signal("changetime",time)
 
-func _on_enemy_removed(_enemy):
-	enemy_count = max(0, enemy_count - 1)
+	cull_distant_enemies()
+
+	# Conta quantos inimigos existem atualmente
+	var current_enemies := get_enemy_count()
+
+	# Para cada tipo
+	for spawn_data in spawns:
+
+		# Ativo após o tempo definido
+		if time >= spawn_data.time_start:
+
+			# Delay normal
+			if spawn_data.spawn_delay_counter < spawn_data.enemy_spawn_delay:
+				spawn_data.spawn_delay_counter += 1
+				continue
+			
+			spawn_data.spawn_delay_counter = 0
+
+			# Spawna, mas respeitando o limite total
+			var amount_to_spawn := spawn_data.enemy_num
+
+			for n in amount_to_spawn:
+				if current_enemies >= max_enemies:
+					break
+
+				var enemy = spawn_data.enemy.instantiate()
+				var pos = get_random_position()
+
+				if is_position_valid(pos):
+					enemy.global_position = pos
+					add_child(enemy)
+					current_enemies += 1
+
+	emit_signal("changetime", time)
+
+
+func get_enemy_count() -> int:
+	var count := 0
+	for c in get_children():
+		if c is CharacterBody2D:
+			count += 1
+	return count
+
 
 func cull_distant_enemies():
 	for child in get_children():
 		if child is CharacterBody2D:
-			var distance = child.global_position.distance_to(player.global_position)
-			if distance > cull_distance:
+			var dist = child.global_position.distance_to(player.global_position)
+			if dist > cull_distance:
 				child.queue_free()
-				enemy_count = max(0, enemy_count - 1)
+
 
 func is_position_valid(pos: Vector2) -> bool:
 	if player.map_bounds.left != 0 and player.map_bounds.right != 0:
@@ -56,30 +74,25 @@ func is_position_valid(pos: Vector2) -> bool:
 			   pos.y >= player.map_bounds.top and pos.y <= player.map_bounds.bottom
 	return true
 
+
 func get_random_position():
-	var vpr = get_viewport_rect().size * randf_range(1.1,1.4)
-	var top_left = Vector2(player.global_position.x - vpr.x/2, player.global_position.y - vpr.y/2)
-	var top_right = Vector2(player.global_position.x + vpr.x/2, player.global_position.y - vpr.y/2)
-	var bottom_left = Vector2(player.global_position.x - vpr.x/2, player.global_position.y + vpr.y/2)
-	var bottom_right = Vector2(player.global_position.x + vpr.x/2, player.global_position.y + vpr.y/2)
-	var pos_side = ["up","down","right","left"].pick_random()
-	var spawn_pos1 = Vector2.ZERO
-	var spawn_pos2 = Vector2.ZERO
-	
-	match pos_side:
+	var vpr = get_viewport_rect().size * randf_range(1.2, 1.5)
+
+	var left = player.global_position.x - vpr.x/2
+	var right = player.global_position.x + vpr.x/2
+	var top = player.global_position.y - vpr.y/2
+	var bottom = player.global_position.y + vpr.y/2
+
+	var side = ["up","down","right","left"].pick_random()
+
+	match side:
 		"up":
-			spawn_pos1 = top_left
-			spawn_pos2 = top_right
+			return Vector2(randf_range(left, right), top)
 		"down":
-			spawn_pos1 = bottom_left
-			spawn_pos2 = bottom_right
-		"right":
-			spawn_pos1 = top_right
-			spawn_pos2 = bottom_right
+			return Vector2(randf_range(left, right), bottom)
 		"left":
-			spawn_pos1 = top_left
-			spawn_pos2 = bottom_left
-	
-	var x_spawn = randf_range(spawn_pos1.x, spawn_pos2.x)
-	var y_spawn = randf_range(spawn_pos1.y,spawn_pos2.y)
-	return Vector2(x_spawn,y_spawn)
+			return Vector2(left, randf_range(top, bottom))
+		"right":
+			return Vector2(right, randf_range(top, bottom))
+
+	return player.global_position
